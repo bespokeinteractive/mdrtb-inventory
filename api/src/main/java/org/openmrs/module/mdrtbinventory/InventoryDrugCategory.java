efbb2f1ad03399874008123823cd70de8a7542c9
Dennys Henry <daugmaud@gmail.com>
2017-11-23 17:20:04 +0300

    Created Inventory Drug Classes and Mapped the to Hibernate

---
+++ api/src/main/java/org/openmrs/module/mdrtbinventory/InventoryDrugCategory.java
@@ -0,0 +1,105 @@
+package org.openmrs.module.mdrtbinventory;
+
+import org.openmrs.User;
+import org.openmrs.api.context.Context;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Dennys Henry
+ * Created on 11/23/2017.
+ */
+public class InventoryDrugCategory
+        implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+    private String description;
+    private Date createdOn;
+    private User createdBy;
+    private Boolean voided;
+    private Date voidedOn;
+    private User voidedBy;
+    private String voidReason;
+
+    public InventoryDrugCategory() {
+        this.voided = false;
+
+        this.createdOn = new Date();
+        this.createdBy = Context.getAuthenticatedUser();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Boolean getVoided() {
+        return voided;
+    }
+
+    public void setVoided(Boolean voided) {
+        this.voided = voided;
+    }
+
+    public Date getVoidedOn() {
+        return voidedOn;
+    }
+
+    public void setVoidedOn(Date voidedOn) {
+        this.voidedOn = voidedOn;
+    }
+
+    public User getVoidedBy() {
+        return voidedBy;
+    }
+
+    public void setVoidedBy(User voidedBy) {
+        this.voidedBy = voidedBy;
+    }
+
+    public String getVoidReason() {
+        return voidReason;
+    }
+
+    public void setVoidReason(String voidReason) {
+        this.voidReason = voidReason;
+    }
+}
---
+++ api/src/main/java/org/openmrs/module/mdrtbinventory/InventoryDrugFacility.java
@@ -0,0 +1,126 @@
+package org.openmrs.module.mdrtbinventory;
+
+import org.openmrs.Location;
+import org.openmrs.User;
+import org.openmrs.api.context.Context;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Dennys Henry
+ * Created on 11/23/2017.
+ */
+public class InventoryDrugFacility
+        implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private InventoryDrug drug;
+    private Location location;
+    private Double available;
+    private Double reorder;
+    private Date createdOn;
+    private User createdBy;
+    private Boolean voided;
+    private Date voidedOn;
+    private User voidedBy;
+    private String voidReason;
+
+    public InventoryDrugFacility(){
+        this.voided = false;
+        this.reorder = 0.0;
+        this.available = 0.0;
+
+        this.createdOn = new Date();
+        this.createdBy = Context.getAuthenticatedUser();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public InventoryDrug getDrug() {
+        return drug;
+    }
+
+    public void setDrug(InventoryDrug drug) {
+        this.drug = drug;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Double getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Double available) {
+        this.available = available;
+    }
+
+    public Double getReorder() {
+        return reorder;
+    }
+
+    public void setReorder(Double reorder) {
+        this.reorder = reorder;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Boolean getVoided() {
+        return voided;
+    }
+
+    public void setVoided(Boolean voided) {
+        this.voided = voided;
+    }
+
+    public Date getVoidedOn() {
+        return voidedOn;
+    }
+
+    public void setVoidedOn(Date voidedOn) {
+        this.voidedOn = voidedOn;
+    }
+
+    public User getVoidedBy() {
+        return voidedBy;
+    }
+
+    public void setVoidedBy(User voidedBy) {
+        this.voidedBy = voidedBy;
+    }
+
+    public String getVoidReason() {
+        return voidReason;
+    }
+
+    public void setVoidReason(String voidReason) {
+        this.voidReason = voidReason;
+    }
+}
